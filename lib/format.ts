4c5ef42b8460ec89547aa2798f655cd865e7c3d6
KnowYourselves <rsalvarez@uc.cl>
2020-10-29 20:21:52 -0300

    fix(format): made options argument optional

--- lib/format.ts
+++ lib/format.ts
@@ -3,7 +3,7 @@ export type FormatOptions = Record<string, unknown>;
 /** formats a rut */
 function format(
   dirtyRut: string | number,
-  options: FormatOptions,
+  options?: FormatOptions,
 ): string {
   // TODO
   return "1.234.456-K";
